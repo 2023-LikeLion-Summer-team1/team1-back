37087e7db7d7e149aa99a14a9bb88371b7dd04b2
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-07-07 00:37:36 +0900

    make springboot project

---
+++ funddigging/src/main/java/com/team1/funddigging/FunddiggingApplication.java
@@ -0,0 +1,13 @@
+package com.team1.funddigging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunddiggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunddiggingApplication.class, args);
+	}
+
+}
