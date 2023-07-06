e726cd6afcf11cb3ca65b285d0f4132c426ee97b
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-07-06 23:47:35 +0900

    make springboot project

---
+++ funddigging/src/test/java/com/team1/funddigging/FunddiggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.funddigging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunddiggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
