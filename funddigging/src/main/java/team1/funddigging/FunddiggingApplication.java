c8b97eb681a6f8643bb5b9dd8ed6cb4ae740b988
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-07-10 21:38:11 +0900

    프로젝트 생성

---
+++ funddigging/src/main/java/team1/funddigging/FunddiggingApplication.java
@@ -0,0 +1,13 @@
+package team1.funddigging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunddiggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunddiggingApplication.class, args);
+	}
+
+}
